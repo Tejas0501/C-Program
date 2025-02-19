436d6ef455f96d5bc9f55df039c0384fdacc9c86
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-19 18:16:19 +0530

    Espace Sequence program

---
+++ Demo2.cpp
@@ -0,0 +1,14 @@
+//escape Sequences
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("Tejas\nRathod");// \n means line changed
+  printf("\nTejas\tRathod");// \t means space 
+  printf("\n\"Tejas Rathod\"");// \" mean double ""
+  printf("\n\'Tejas Rathod\'");// \' mean single ''
+  printf("\n'tejas rathod'"); // we can directly use '' qoutes 
+
+
+  getch();
+}

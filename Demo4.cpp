164ce1c98e2d776dcc38eff2ffb74cc9c044fa5d
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-19 18:29:42 +0530

    Unique name for same data type

---
+++ Demo4.cpp
@@ -0,0 +1,14 @@
+//Unique name for same datatype 
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    int y=20;
+
+    printf("x=%i\n",x);
+
+    printf("y=%i",y);
+
+    getch();
+}

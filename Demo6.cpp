cbf85aa395afdee63fa3bf4bf6ff5274383836bc
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-19 18:57:15 +0530

    Arithmetic operator Program

---
+++ Demo6.cpp
@@ -0,0 +1,18 @@
+// Arithmetic operator 
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    int y=7;
+
+    printf("%i\n",x+y);
+    printf("%i\n",x-y);
+    printf("%i\n",x*y);
+    printf("%i\n",x/y);
+    printf("%i\n",x%y);
+
+    getch();
+
+}

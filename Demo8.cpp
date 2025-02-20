beb536de37faa9be41c9fd4252c0195f984db1c7
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-20 22:17:02 +0530

    Number System

---
+++ Demo8.cpp
@@ -0,0 +1,15 @@
+// Number System 
+// Decimal to binary or octal or hexa decimal we do divide 
+#include<conio.h>
+#include<stdio.h>
+
+int main()
+{
+    int x=25;
+
+    printf("%i\n",x);// i means integer
+    printf("%d\n",x);// d means decimal
+    printf("%o\n",x);// o means octal
+
+    getch();
+}

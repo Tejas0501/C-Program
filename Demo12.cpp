d0b0c308083dbb07c0624d46fe38d7c928b1ae51
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-20 22:55:18 +0530

    Increament and Decreament Operator

---
+++ Demo12.cpp
@@ -0,0 +1,17 @@
+// Increment and Decrement Operator 
+/*
+x++ = x+1;
+x-- = x-1;
+*/
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    printf("%d\n",x);
+    x++;
+    printf("%d\n",x);
+    ++x;
+    printf("%d\n",x);
+    getch();
+}

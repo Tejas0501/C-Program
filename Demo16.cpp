4f89679b662a747444f1ec39372d19b7c971c41a
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:52:46 +0530

    chech the number is even or odd

---
+++ Demo16.cpp
@@ -0,0 +1,19 @@
+// Check the number is even or odd
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=11;
+
+    if(x%2==0)
+    {
+        printf("The number is even");
+    }
+    if(x%2==1)
+    {
+        printf("The number is odd");
+    }
+
+    getch();
+}

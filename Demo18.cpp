24e842900cedd9857b8b315972921a5fcaa4b87c
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:35:25 +0530

    Find Larget number using ifelse

---
+++ Demo18.cpp
@@ -0,0 +1,20 @@
+// Find largest number using ifelse statement 
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=20;
+    int y=40;
+
+    if(x>y)
+    {
+        printf("x is largest");
+    }
+    else
+    {
+        printf("y is largest");
+    }
+
+    getch();
+}

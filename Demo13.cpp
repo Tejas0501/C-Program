a933995f99c6d288aecc003833076323d27cd962
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:06:44 +0530

    Simple if understanding program

---
+++ Demo13.cpp
@@ -0,0 +1,17 @@
+//if statement
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    if(0)//0 means false
+    {
+        printf("False");
+    }
+    if(1)
+    {
+        printf("True");
+    }
+
+    getch();
+}

655ca465bd28915f90ad4bdb1ebe07b471ec68b8
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:15:55 +0530

    The number is positive or negative

---
+++ Demo15.cpp
@@ -0,0 +1,19 @@
+// find the number is positive or negative
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=-10;
+
+    if(x>=0)
+    {
+      printf("x is positive");
+    }
+    if(x<0)
+    {
+        printf("x is negative");
+    }
+
+    getch();
+}

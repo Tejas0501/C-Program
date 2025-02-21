0e3d5f7060ac440f3b5b9ccf907605385a59b506
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:11:57 +0530

    Largest Number between two number

---
+++ Demo14.cpp
@@ -0,0 +1,20 @@
+// Find largest Number between two number
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=200;
+    int y=30;
+
+    if(x>y)
+    {
+      printf("x is largest");
+    }
+    if(y>x)
+    {
+        printf("Y is largest");
+    }
+
+    getch();
+}

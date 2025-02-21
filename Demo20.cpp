1c0e4ff980ac7c4d585c20ec0751875c799172f9
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 23:01:56 +0530

    Logical &&(And) Operator Working

---
+++ Demo20.cpp
@@ -0,0 +1,16 @@
+// Logical Operator 
+// && (AND operator)
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+
+// In &&(and) operator if one conditioin is false the output is false
+
+    printf("%d\n",0&&0);//false
+    printf("%d\n",0&&1);//false
+    printf("%d\n",1&&0);//false
+    printf("%d\n",1&&1);//true
+
+    getch();
+}

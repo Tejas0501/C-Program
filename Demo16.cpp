f451208852481e960075b9367839d8738fa21d0d
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:23:51 +0530

    Check the no is even or odd

---
+++ Demo16.cpp
@@ -0,0 +1,20 @@
+// Check the number is positive or negative
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=4;
+
+    if(x%2==0)
+    {
+       printf("The no is even");
+    }
+    if(x%2==1)
+    {
+        printf("The no is odd");
+    }
+
+    getch();
+
+}

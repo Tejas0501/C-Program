3eb947a864d219bafab9d75b1fe02c3d80e909ed
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-21 22:31:23 +0530

    if else statemant program

---
+++ Demo17.cpp
@@ -0,0 +1,20 @@
+// If else statement 
+
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int x=-10;
+
+    if(x<0)
+    {
+        printf("The number is negative");
+    }
+    else
+    {
+        printf("The number is positive");
+    }
+
+    getch();
+
+}

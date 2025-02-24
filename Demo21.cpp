162068b8cd9bcfe0231bd764818f253807c6afb9
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-24 16:34:48 +0530

    Logical or Operator

---
+++ Demo21.cpp
@@ -0,0 +1,15 @@
+// logical or operator 
+// ||(Or operator)
+
+//In this operator if one condition is true then output is true
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    printf("%d\n",0||0);//false
+    printf("%d\n",0||1);//true
+    printf("%d\n",1||0);//true
+    printf("%d\n",1||1);//true
+
+    getch();
+}

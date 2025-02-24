a20333c3398987617bdd12e246bb252be8cd6940
Tejas Rathod <tejasrathod2t005@gmail.com>
2025-02-24 17:15:22 +0530

    Goto statement Program

---
+++ Demo25.cpp
@@ -0,0 +1,16 @@
+// Goto statement
+
+// It help us to devide the program into parts
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    printf("Hello\n");
+    goto z;
+    printf("How are you\n");
+
+    z:
+    printf("I Love India\n");
+
+    getch();
+}
